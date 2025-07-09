efcbd6fe5d63f968c3c62e0425e49b6bc6feb308
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-09 18:42:12 -0500

    Step 5: Added a side nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css'
 import CoinInfo from "./components/CoinInfo"
+import SideNav from "./components/SideNav"
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
@@ -85,6 +86,7 @@ function App() {
                 ))}
         </ul>
       </div>
+      <SideNav />
     </div>
   )
 }
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,10 @@
+function SideNav () {
+    return (
+        <div className="sidenav">
+            <h1>here is my side nav bar</h1>
+        </div>
+    )
+
+}
+
+export default SideNav
